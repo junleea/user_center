73d833afeb0bbca7478df32b97ebdef843d08d08
lijun <lijun.ls@qq.com>
2025-10-12 13:36:50 +0800

    添加密码登录时TOTP二次认证

--- dao/user.go
+++ dao/user.go
@@ -427,7 +427,7 @@ func DelUserTOTPSecret(user_id uint) error {
 	} else {
 		db = DB
 	}
-	res := db.Delete(&TOTPSecretInfo{}, user_id)
+	res := db.Where("deleted_at IS NULL").Delete(&TOTPSecretInfo{}, user_id)
 	return res.Error
 }
 func GetDB() *gorm.DB {
