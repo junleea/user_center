73148c50986e29d530cc337556e41b1ef4cdd1c0
lijun <lijun.ls@qq.com>
2025-10-18 22:47:18 +0800

    更新用户信息

--- dao/user.go
+++ dao/user.go
@@ -439,7 +439,7 @@ func DelUserTOTPSecret(user_id uint) error {
 	} else {
 		db = DB
 	}
-	res := db.Where("deleted_at IS NULL and type = 0 and user_id = ?", user_id).Delete(&TOTPSecretInfo{})
+	res := db.Where("deleted_at IS NULL and user_id = ? and type = 0 ", user_id).Delete(&TOTPSecretInfo{})
 	return res.Error
 }
 
